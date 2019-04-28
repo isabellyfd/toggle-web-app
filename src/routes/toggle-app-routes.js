b58429de610a9c0397f759bbfc4512f3dfafa64e
isadamascena <idamasce@thoughtworks.com>
2019-04-27 21:55:05 -0300

    Adds logs to the routes

--- src/routes/toggle-app-routes.js
+++ src/routes/toggle-app-routes.js
@@ -1,38 +1,45 @@
 const { Router } = require('express');
 const service =  require('../services/toggle-service');
-
+const winston = require('winston');
 const router = Router();
 
 router.post('/v1/toggle-service/create-application/', (req, res) => {
-    if (req.body.name === undefined){
-        res.status(400).send('Request without name attribute on body');
+    const name  = req.body.name;
+    const userId = req.body.userId;
+
+    if (name === undefined || userId === undefined){
+        winston.info(`Bad request with body parameters name = ${name} and userId = ${userId}`);
+        res.status(400).send('Request without the required attributes on body');
         return;
     }
-    service.createApplicationForUserwithName(req.body.userId, req.body.name)
+    service.createApplicationForUserwithName(userId, name)
         .then(() => {
+            winston.info(`application with name = ${name} created for user with id = ${userId}`);
             res.sendStatus(200);
         })
         .catch(error => {
+            winston.info(`application creation failed name = ${name} for user with id = ${userId}`);
             res.sendStatus(500);
         });
 });
 
 router.post('/v1/toggle-service/create-user/', (req, res) => {
-    if(req.body.email === undefined){
-        res.status(400).send('Request without email attribute on body');
-        return;
-    }
+    const email = req.body.email;
+    const password = req.body.password;
 
-    if(req.body.password === undefined){
-        res.status(400).send('Request without password attribute on body');
+    if(email === undefined || password === undefined){
+        winston.info(`Bad request with body parameters email = ${email} and password = ${password}`);
+        res.status(400).send('Request without the required attributes on body');
         return;
     }
 
     service.createNewUser(req.body.email, req.body.password)
         .then(response => {
+            winston.info(`created user successfully with email = ${email}`);
             res.json(response);
         })
         .catch(error => {
+            winston.info(`failed to create user with email = ${email}`);
             res.status(500).send(error.code);
         });
 });
@@ -40,7 +47,8 @@ router.post('/v1/toggle-service/create-user/', (req, res) => {
 router.get('/v1/toggle-service/my-apps/:userId', (req, res) => {
     const userId = req.params.userId;
     if (userId === undefined) {
-        res.sendStatus(500);
+        winston.info(`Bad request with parameters userId = ${userId}`);
+        res.sendStatus(400);
         return;
     }
     
@@ -63,9 +71,11 @@ router.get('/v1/toggle-service/my-apps/:userId', (req, res) => {
 router.post('/v1/toggle-service/signin/', (req, res) => {
     service.signInWith(req.body.email, req.body.password)
         .then(response => {
+            winston.info(`signin sucessful`);
             res.json(response);
         })
         .catch(error =>{
+            winston.info(`could not signin`);
             res.status(500).send(error.code);
         });
 });
